df616cc8326e6e99123e325730b96a942c16e280
akadouri <ariel@carmera.co>
2023-01-02 23:08:28 -0500

    modify script to use osm2pgsql-replication

--- osm2pgsql/osm-importer.sh
+++ osm2pgsql/osm-importer.sh
@@ -1,70 +1,48 @@
 #!/bin/bash
-# Original author: Rex Tsai <rex.cc.tsai@gmail.com> https://github.com/OsmHackTW/osm2pgsql-docker
 
-echo REGION=${REGION:="asia/taiwan"}
-echo COUNTRY=${COUNTRY:=$(basename $REGION)}
-echo DATADIR=${DATADIR:="/osm"}
-echo PBF=${PBF:=$DATADIR/${COUNTRY}-latest.osm.pbf}
-echo LOOP=${LOOP:=600}
+export PGPASSWORD=$PG_ENV_POSTGRES_PASSWORD
+echo DATADIR=${DATADIR:="/osm/data"}
+echo PBF=${PBF:=$DATADIR/$(echo $REGION | grep -o '[^/]*$')-latest.osm.pbf}
 HOST=download.geofabrik.de
 
-[ -d $DATADIR ] || (echo "$DATADIR not found" && exit -1)
-
-if [ -z $PG_ENV_POSTGRES_PASSWORD ] \
-    || [ -z $PG_ENV_POSTGRES_DB ] \
-    || [ -z $PG_ENV_POSTGRES_USER ] \
-    || [ -z $PG_PORT_5432_TCP_ADDR ] \
-    || [ -z $PG_PORT_5432_TCP_PORT ] ; then
-        echo "missing Progress settings"
-
-cat <<EOF
-PG_PORT_5432_TCP_ADDR=$PG_PORT_5432_TCP_ADDR
-PG_PORT_5432_TCP_PORT=$PG_PORT_5432_TCP_PORT
-PG_ENV_POSTGRES_DB=$PG_ENV_POSTGRES_DB
-PG_ENV_POSTGRES_USER=$PG_ENV_POSTGRES_USER
-PG_ENV_POSTGRES_PASSWORD=$PG_ENV_POSTGRES_PASSWORD
-EOF
-exit 1
-fi
-
-function importosm () {
-    # ping database.
-    echo "SELECT 1;" | PGPASSWORD=$PG_ENV_POSTGRES_PASSWORD \
-        psql --no-password \
-        -h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT \
-        -U $PG_ENV_POSTGRES_USER $PG_ENV_POSTGRES_DB \
-    || return $?
-
-    echo "CREATE EXTENSION hstore;" | PGPASSWORD=$PG_ENV_POSTGRES_PASSWORD \
-        psql --no-password \
-        -h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT \
-        -U $PG_ENV_POSTGRES_USER $PG_ENV_POSTGRES_DB \
-
-    UPDATEPBF=$(mktemp -p $DATADIR XXX.pbf)
-    if [ ! -f ${PBF} ] ; then
-        wget -O "${UPDATEPBF}" http://$HOST/${REGION}-latest.osm.pbf \
-            || return $?
+if psql --no-password -h $PG_PORT_5432_TCP_ADDR -U $PG_ENV_POSTGRES_USER $PG_ENV_POSTGRES_DB -c "select * from planet_osm_replication_status;"; then
+    echo "Updating."
+    osm2pgsql-replication update \
+        -v \
+        --host $PG_PORT_5432_TCP_ADDR \
+        --database $PG_ENV_POSTGRES_DB \
+        --username $PG_ENV_POSTGRES_USER \
+        -- -k --style /user/local/bin/custom.style --extra-attributes
+else
+    echo "Database not ready, need to intialize."
+    if [[ -f "$PBF" ]]; then
+        echo "Using local file at $PBF"
     else
-        osmupdate -v --base-url=$HOST/${REGION}-updates "$PBF" "$UPDATEPBF" \
-            || return $?
-    fi 
-    trap "Importing in progress, ignored SIGINT & SIGTERM." SIGINT SIGTERM
-    PGPASSWORD=$PG_ENV_POSTGRES_PASSWORD \
-        osm2pgsql -k \
+        echo "$PBF File not found, downloading..."
+        wget -O "${PBF}" http://$HOST/${REGION}-latest.osm.pbf
+    fi
+
+    psql --no-password \
+        -h $PG_PORT_5432_TCP_ADDR -p $PG_PORT_5432_TCP_PORT \
+        -U $PG_ENV_POSTGRES_USER $PG_ENV_POSTGRES_DB \
+        -c "CREATE EXTENSION hstore"
+
+    osm2pgsql -v \
+        -k \
         --create \
         --slim \
-        --cache 2000 \
+        --cache 4000 \
         --extra-attributes \
         --style /user/local/bin/custom.style \
         --host $PG_PORT_5432_TCP_ADDR \
         --database $PG_ENV_POSTGRES_DB \
         --username $PG_ENV_POSTGRES_USER \
         --port $PG_PORT_5432_TCP_PORT \
-        $UPDATEPBF && mv -v $UPDATEPBF $PBF
-}
+        $PBF
 
-while : ; do
-    importosm
-    [ $LOOP -eq 0 ] && exit $?
-    sleep $LOOP || exit
-done
+    osm2pgsql-replication init \
+        --host $PG_PORT_5432_TCP_ADDR \
+        --database $PG_ENV_POSTGRES_DB \
+        --username $PG_ENV_POSTGRES_USER \
+        --port $PG_PORT_5432_TCP_PORT --osm-file $PBF
+fi
